4c5813f80f674b86744e067058c41552d1662460
marinaoleinik <marina.oleinik@tthk.ee>
2025-04-09 17:01:19 +0300

    Funktsioonid on eradi klassis kirjutatud

--- MainClass.cs
+++ MainClass.cs
@@ -18,60 +18,25 @@ namespace TARgv24_C
             int a = 0;
             string tekst = "Python";
             char taht= 'A';
-            double arv = 5.4536237287;
-            float arv1 = 2;
+            double arv = Convert.ToDouble(Console.ReadLine());
+            //double.TryParse(Console.ReadLine(), out double arv)
+            float arv1 = (float)Convert.ToDecimal(Console.ReadLine());
             Console.Write("Mis on sinu nimi? ");
             tekst = Console.ReadLine();
             Console.WriteLine("Tere!\n"+tekst);
             Console.WriteLine("Tere!\n {0}",tekst);
-            if (tekst.ToLower() == "juku")
-            {
-                Console.WriteLine("Lahme kinno!");
-                try
-                {
-                    Console.WriteLine("{0}\n Kui vana sa oled?", tekst);
-                    int vanus=int.Parse(Console.ReadLine());
-                    if (vanus<=0 || vanus>100) //&& - and, || - or
-                    {
-                        Console.WriteLine("Viga!");
-                    }
-                    else if (vanus>0 && vanus <=6)
-                    {
-                        Console.WriteLine("Tasuta");
-                    }
-                    else if (vanus<=15)
-                    {
-                        Console.WriteLine("Lastepilet");
-                    }
-                    else if (vanus <= 65)
-                    { 
-                        Console.WriteLine("Täispilet!"); 
-                    }
-                    else if (vanus <= 100)
-                    { 
-                        Console.WriteLine("Sooduspilet!");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Olen hõivatud!");
-            }
-
-
-
+            FunktsioonideClass.Juku(tekst);
             Console.Write("Arv 2: ");
             int arv2=int.Parse(Console.ReadLine());
             //Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
             arv1=FunktsioonideClass.Kalkulaator(a, arv2);
             Console.WriteLine(arv1);
-
-
-
+            Console.WriteLine("Switch'i kasutamine");
+            Random rnd = new Random();
+            a=rnd.Next(1,7);
+            Console.WriteLine(a);
+            FunktsioonideClass.Nadala_paev(a);
+            Console.WriteLine(tekst);
             Console.ReadKey();
         }
 
